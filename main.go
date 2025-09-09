20eeff535c3842964c3e04bc6cc0d8051b529ba5
dbarrantes1 <162936406+dbarrantes1@users.noreply.github.com>
2025-09-08 21:40:43 -0600

    Algunas funciones

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -18,16 +19,19 @@ type Stack struct {
 	items []any
 }
 
-type variablesMemory struct { //Es lo mismo al anterior struct, estas separadas solo para evitar utilizar los atributos de la pila en la memoria de variables
-	nombre any
+type variablesMemory struct { //Es lo mismo al anterior struct, estas separadas
+	// solo para evitar utilizar los atributos de la pila en la memoria de variables
+	nombre   any
 	variable any
 }
 
 var stack Stack
-type varMemory []variablesMemory
-var varMem varMemory 
-//var varIndex map[any]int
 
+type varMemory []variablesMemory
+
+var varMem varMemory
+
+//var varIndex map[any]int
 
 /*------------------------------------------------------------------------
 ---------------------------Atributos de la pila---------------------------
@@ -37,14 +41,14 @@ func (s *Stack) push(v any) {
 	s.items = append(s.items, v)
 }
 
-func (s *Stack) pop() (bool) {
+func (s *Stack) pop() bool {
 	if len(s.items) == 0 {
 		return false
 	}
 	i := len(s.items) - 1
 	//v := s.items[i]
 	s.items = s.items[:i]
-	return  true
+	return true
 }
 
 func (s *Stack) top() (any, bool) {
@@ -115,7 +119,7 @@ func lecturaByteCode(text string) {
 			instruccionString = ""
 			instruccionRune = []rune{}
 			readingInts = false
-			
+
 		} else if chr == '\t' && readingInts {
 			instruccionString = string(instruccionRune)
 			readingInts = false
@@ -196,25 +200,236 @@ func whichExecute(instruccion string, item any) {
 		EXECUTE_LOAD_CONST(item)
 	case "STORE_FAST":
 		varMem.EXECUTE_STORE_FAST(item)
+	case "BINARY_MULTIPLY":
+		EXECUTE_BINARY_MULTIPLY()
+
 	}
 }
 
-//Coloca el valor de la constante en el tope de la pila 
+// Coloca el valor de la constante en el tope de la pila
 func EXECUTE_LOAD_CONST(item any) {
 	stack.push(item)
 	//fmt.Println(stack.items[0])
 }
 
-//Escribe el contenido del tope de la pila en la variable
+// Escribe el contenido del tope de la pila en la variable
 func (varMe *varMemory) EXECUTE_STORE_FAST(varname any) {
 	//var index = len(*varMe)-1
 	//varIndex[varname] = index
-	variableItem,top := stack.top()
-	if(!top){
+	variableItem, top := stack.top()
+	if !top {
 		panic("(Error) No se puede almacenar variables sin un tope en la pila")
 	}
 	*varMe = append(*varMe, variablesMemory{nombre: varname, variable: variableItem})
 	stack.pop()
+}
+
+// Realiza la multiplicación de los dos valores en el tope de la pila
+func EXECUTE_BINARY_MULTIPLY() {
+	if len(stack.items) < 2 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar multiplicación")
+	}
+
+	toFloat := func(val any) float64 {
+		switch v := val.(type) {
+		case int:
+			return float64(v)
+		case float32:
+			return float64(v)
+		case float64:
+			return v
+		default:
+			panic("(Error) Tipo no soportado en multiplicación")
+		}
+	}
+
+	val2, _ := stack.top()
+	stack.pop()
+	val1, _ := stack.top()
+	stack.pop()
+
+	n1 := toFloat(val1)
+	n2 := toFloat(val2)
+	stack.push(n1 * n2)
+}
+
+// Realiza la división de los dos valores en el tope de la pila
+func EXECUTE_BINARY_DIVIDE() {
+	if len(stack.items) < 2 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar división")
+	}
+
+	toFloat := func(val any) float64 {
+		switch v := val.(type) {
+		case int:
+			return float64(v)
+		case float32:
+			return float64(v)
+		case float64:
+			return v
+		default:
+			panic("(Error) Tipo no soportado en división")
+		}
+	}
+	val2, _ := stack.top()
+	stack.pop()
+	val1, _ := stack.top()
+	stack.pop()
+
+	n1 := toFloat(val1)
+	n2 := toFloat(val2)
+
+	if n2 == 0 {
+		panic("(Error) No se puede dividir entre 0")
+	}
+
+	stack.push(n1 / n2)
+}
+
+// Realiza la operación AND entre los dos valores en el tope de la pila
+func EXECUTE_BINARY_AND() {
+	if len(stack.items) < 2 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar AND")
+	}
+
+	toBool := func(val interface{}) bool {
+		b, ok := val.(bool)
+		if !ok {
+			panic("(Error) Tipo no soportado en AND")
+		}
+		return b
+	}
+
+	val1, _ := stack.top()
+	stack.pop()
+	val2, _ := stack.top()
+	stack.pop()
+
+	stack.push(toBool(val1) && toBool(val2))
+}
+
+// Realiza la operación OR entre los dos valores en el tope de la pila
+func EXECUTE_BINARY_OR() {
+	if len(stack.items) < 2 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar OR")
+	}
+
+	toBool := func(val interface{}) bool {
+		b, ok := val.(bool)
+		if !ok {
+			panic("(Error) Tipo no soportado en OR")
+		}
+		return b
+	}
+
+	val1, _ := stack.top()
+	stack.pop()
+	val2, _ := stack.top()
+	stack.pop()
+
+	stack.push(toBool(val1) || toBool(val2))
+}
+
+// Realiza la operación MODULO entre los dos valores en el tope de la pila
+func EXECUTE_BINARY_MODULO() {
+	if len(stack.items) < 2 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar modulo")
+	}
+
+	toFloat := func(val any) float64 {
+		switch v := val.(type) {
+		case int:
+			return float64(v)
+		case float32:
+			return float64(v)
+		case float64:
+			return v
+		default:
+			panic("(Error) Tipo no soportado en modulo")
+		}
+	}
+	val2, _ := stack.top()
+	stack.pop()
+	val1, _ := stack.top()
+	stack.pop()
+
+	n1 := toFloat(val1)
+	n2 := toFloat(val2)
+
+	if n2 == 0 {
+		panic("(Error) No se puede dividir entre 0")
+	}
+
+	stack.push(math.Mod(n1, n2))
+}
+
+// Construye una lista con los n elementos del tope de la pila
+func EXECUTE_BUILD_LIST(elements int) {
+	if len(stack.items) < elements {
+		panic("(Error) No hay suficientes elementos en la pila para construir la lista")
+	}
+	list := make([]any, elements)
+	for i := 1; i <= elements; i++ {
+		val, _ := stack.top()
+		stack.pop()
+		list[elements-i] = val
+	}
+	stack.push(list)
+}
+
+// Realiza la subscripción de una lista
+func EXECUTE_BINARY_SUBSCR() {
+	if len(stack.items) < 2 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar subscripción")
+	}
+	index, _ := stack.top()
+	stack.pop()
+	list, _ := stack.top()
+	stack.pop()
+
+	listSlice, ok := list.([]any)
+	if !ok {
+		panic("(Error) El elemento no es una lista")
+	}
+
+	idx, ok := index.(int)
+	if !ok {
+		panic("(Error) El índice no es un entero")
+	}
+
+	if idx < 0 || idx >= len(listSlice) {
+		panic("(Error) Índice fuera de rango")
+	}
+
+	stack.push(listSlice[idx])
+}
+
+// Realiza la asignación por subscripción de una lista
+func EXECUTE_STORE_SUBSCR() {
+	if len(stack.items) < 3 {
+		panic("(Error) No hay suficientes elementos en la pila para realizar asignación por subscripción")
+	}
+	index, _ := stack.top()
+	stack.pop()
+	list, _ := stack.top()
+	stack.pop()
+	value, _ := stack.top()
+	stack.pop()
+
+	listSlice, ok := list.([]any)
+	if !ok {
+		panic("(Error) El elemento no es una lista")
+	}
+
+	idx, ok := index.(int)
+	if !ok {
+		panic("(Error) El índice no es un entero")
+	}
+
+	if idx < 0 || idx >= len(listSlice) {
+		panic("(Error) Índice fuera de rango")
+	}
+	listSlice[idx] = value
 }
 
 /*------------------------------------------------------------------------
@@ -222,7 +437,7 @@ func (varMe *varMemory) EXECUTE_STORE_FAST(varname any) {
 ------------------------------------------------------------------------*/
 
 func main() {
-	var txt string = "Pruebas_de_interprete\\example1.txt"
+	var txt string = "Pruebas_de_interprete/example1.txt"
 	fileError, errorName := fileExist(txt)
 	if fileError {
 		fmt.Println("Si va")
@@ -239,6 +454,7 @@ func main() {
 	fmt.Println(any(tal))
 	/*tal := '0'
 	fmt.Println(tal)*/
+
 }
 
 /*Prueba
